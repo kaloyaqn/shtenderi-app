d2ae625795b72f49352f39d34b035467fc2802d5
Kaloyan Angelov <64629762+kaloyaqn@users.noreply.github.com>
2025-06-25 15:52:30 +0300

    deploy

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function DashboardHome() {
     return (
         <>
         Начало
+
+        <Link href="http://localhost:3002/dashboard/stands/9dce99cd-a952-4ecf-aca1-348ce06d06dc/revision">
+        rev</Link>
         </>
     )
 }
