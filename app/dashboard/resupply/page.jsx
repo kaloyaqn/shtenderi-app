cd2514b5e5d078b8de9ad516e81aef46f573145b
Kaloyan Angelov <64629762+kaloyaqn@users.noreply.github.com>
2025-09-05 14:48:05 +0300

    working router back

--- app/dashboard/resupply/page.jsx
+++ app/dashboard/resupply/page.jsx
@@ -371,6 +371,7 @@ export default function GeneralResupplyPage() {
         setSelectedProducts([]);
         setBarcodeInput("");
         setStep(1);
+        router.back();
       }
     } catch (error) {
       toast.error(error.message);
